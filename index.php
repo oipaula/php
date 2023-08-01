f15c15aeea0c04b9b3b58598f936cce36794f046
Paula Xavier <91852865+oipaula@users.noreply.github.com>
2023-08-01 14:41:37 -0300

    "Meu Primeiro Site PHP"

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// comentario
+echo "My first PHP script!";
+
+$turma2 = "Vamos desenvolver website em PHP";
+echo "<br>".$turma2;
+
+$x = 5 /* + 15 */ + 5;
+echo "<br>".$x;
+
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+echo "<br>".$txt." x = ".$x." y = ".$y;
+?>
+
+</body>
+</html>
