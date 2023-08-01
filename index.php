ea8ba88e27af4aadc125c41e503a78c4f780c2e9
Paula Xavier <91852865+oipaula@users.noreply.github.com>
2023-08-01 15:05:03 -0300

    Meu Primeiro Site PHP pt.2

--- index.php
+++ index.php
@@ -15,7 +15,26 @@ echo "<br>".$x;
 $txt = "Hello world!";
 $x = 5;
 $y = 10.5;
-echo "<br>".$txt." x = ".$x." y = ".$y;
+echo "<br>".$txt." x = ".$x." y = ".$y."<br>";
+
+$txt = "W3Schools.com";
+// vai dar erro o código abaixo:
+//echo "I love $txt!";
+// consertando o código acima
+echo "<br>"."I love".$txt."!"."<br>";
+
+$x = 5;
+$y = 4;
+echo "<br> O valor da soma é igual a ".$x + $y."<br>";
+var_dump($x + $y)."<br>";
+
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+for ($i=0;$i<3;$i++){
+    echo "<br>".$cars[$i];
+}
+
+
 ?>
 
 </body>
