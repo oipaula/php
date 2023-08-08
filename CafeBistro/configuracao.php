e6b11d444efb0537ea9e14f4bb8757f0c6092bc6
Paula Xavier <91852865+oipaula@users.noreply.github.com>
2023-08-08 15:07:50 -0300

    Add files via upload

---
+++ CafeBistro/configuracao.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cafebistro";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Conectado com sucesso";
+
+$sql = "SELECT * FROM cafe";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+?>
